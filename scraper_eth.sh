2feac39b85fdaee8cb8ff8c881b9030f6936572b
Ubuntu <ubuntu@ip-172-31-31-129.eu-north-1.compute.internal>
2025-03-23 19:01:58 +0100

    Mon message de commit

--- scraper_eth.sh
+++ scraper_eth.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+# Chemin pour le fichier log
 LOGFILE="/home/ubuntu/bitcoin-dashboard/scraper_eth.log"
-echo "$(date) : Début de l'exécution du scraper pour l'Ether" >> $LOGFILE
 
-price=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd" \
-         | grep -oP '"usd":\K[\d.]+')
-echo "$(date) : Prix récupéré = $price" >> $LOGFILE
+echo "$(date +'%Y-%m-%d %H:%M:%S') : Début de l'exécution du scraper pour l'Ether" >> $LOGFILE
 
-echo "$(date +'%Y-%m-%d %H:%M:%S'), $price" >> data_eth.csv
-echo "$(date) : Fin de l'exécution du scraper pour l'Ether" >> $LOGFILE
+# Récupération du prix via l'API CoinGecko
+price=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd" | grep -oP '"usd":\K[\d.]+')
+echo "$(date +'%Y-%m-%d %H:%M:%S') : Prix récupéré = $price" >> $LOGFILE
+
+# Écriture dans le fichier CSV avec chemin absolu
+echo "$(date +'%Y-%m-%d %H:%M:%S'), $price" >> /home/ubuntu/bitcoin-dashboard/data_eth.csv
+
+echo "$(date +'%Y-%m-%d %H:%M:%S') : Fin de l'exécution du scraper pour l'Ether" >> $LOGFILE
